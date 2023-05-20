df7c9f67b2d781f528497346a7b10fa9111632de
Stephanne21 <olesamamev@uch.pe>
2023-05-20 16:10:22 -0500

    año

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Asociación Kaprichos Perrunos
+      Asociación Kaprichos Perrunos 2023
     </main>
   );
 }
